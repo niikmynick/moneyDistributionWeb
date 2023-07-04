892b61bd69bb1ccdc2134278519f67004971016d
fk724s <fk724s@mail.ru>
2023-07-04 13:49:14 +0300

    bug fixes

--- money_distribution_app/urls.py
+++ money_distribution_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
+    path('', views.index, name='main-page'),
+    path('about', views.about, 'about-page'),
     path('account', views.account),
     path('application', views.application),
 ]
